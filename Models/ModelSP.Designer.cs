a87802be858647ea01d8661a05883b3db2ae48ed
schezparro <schezparro@gmail.com>
2024-01-14 22:07:25 -0500

    modificacion en recursos e incidencias

--- Models/ModelSP.Designer.cs
+++ Models/ModelSP.Designer.cs
@@ -1,4 +1,4 @@
-// T4 code generation is enabled for model 'C:\Users\Raciel Schez\SifizPlanning\Models\ModelSP.edmx'. 
+﻿// T4 code generation is enabled for model 'H:\WORK\SifizPlanning\Models\ModelSP.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
